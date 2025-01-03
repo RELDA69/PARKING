5fcaa9459ee1b48d203e68d2247381d50529459f
RELDA <RELDA@DESKTOP-OSCKBRD>
2025-01-03 19:58:13 +0800

    Add project files.

---
+++ PARKING/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace PARKING
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dgvParkingQueue = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAddVehicle = new System.Windows.Forms.Button();
+            this.btnRemoveVehicle = new System.Windows.Forms.Button();
+            this.btnClearQueue = new System.Windows.Forms.Button();
+            this.txtLicensePlate = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvParkingQueue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvParkingQueue
+            // 
+            this.dgvParkingQueue.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dgvParkingQueue.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvParkingQueue.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dgvParkingQueue.Location = new System.Drawing.Point(12, 12);
+            this.dgvParkingQueue.Name = "dgvParkingQueue";
+            this.dgvParkingQueue.Size = new System.Drawing.Size(512, 321);
+            this.dgvParkingQueue.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Slot";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "Vehicle";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.HeaderText = "Status";
+            this.Column3.Name = "Column3";
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(569, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Parking History";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(563, 258);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Reservation List";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(573, 317);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 31);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Parking Queue";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(636, 379);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 31);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Exit";
+            // 
+            // btnAddVehicle
+            // 
+            this.btnAddVehicle.Location = new System.Drawing.Point(66, 386);
+            this.btnAddVehicle.Name = "btnAddVehicle";
+            this.btnAddVehicle.Size = new System.Drawing.Size(97, 41);
+            this.btnAddVehicle.TabIndex = 5;
+            this.btnAddVehicle.Text = "Add Vehicle";
+            this.btnAddVehicle.UseVisualStyleBackColor = true;
+            this.btnAddVehicle.Click += new System.EventHandler(this.btnAddVehicle_Click_1);
+            // 
+            // btnRemoveVehicle
+            // 
+            this.btnRemoveVehicle.Location = new System.Drawing.Point(218, 387);
+            this.btnRemoveVehicle.Name = "btnRemoveVehicle";
+            this.btnRemoveVehicle.Size = new System.Drawing.Size(97, 41);
+            this.btnRemoveVehicle.TabIndex = 6;
+            this.btnRemoveVehicle.Text = "Remove Vehicle";
+            this.btnRemoveVehicle.UseVisualStyleBackColor = true;
+            this.btnRemoveVehicle.Click += new System.EventHandler(this.btnRemoveVehicle_Click);
+            // 
+            // btnClearQueue
+            // 
+            this.btnClearQueue.Location = new System.Drawing.Point(380, 388);
+            this.btnClearQueue.Name = "btnClearQueue";
+            this.btnClearQueue.Size = new System.Drawing.Size(97, 40);
+            this.btnClearQueue.TabIndex = 7;
+            this.btnClearQueue.Text = "Clear Queue";
+            this.btnClearQueue.UseVisualStyleBackColor = true;
+            this.btnClearQueue.Click += new System.EventHandler(this.btnClearQueue_Click_1);
+            // 
+            // txtLicensePlate
+            // 
+            this.txtLicensePlate.Location = new System.Drawing.Point(81, 350);
+            this.txtLicensePlate.Name = "txtLicensePlate";
+            this.txtLicensePlate.Size = new System.Drawing.Size(383, 20);
+            this.txtLicensePlate.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtLicensePlate);
+            this.Controls.Add(this.btnClearQueue);
+            this.Controls.Add(this.btnRemoveVehicle);
+            this.Controls.Add(this.btnAddVehicle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvParkingQueue);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvParkingQueue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvParkingQueue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAddVehicle;
+        private System.Windows.Forms.Button btnRemoveVehicle;
+        private System.Windows.Forms.Button btnClearQueue;
+        private System.Windows.Forms.TextBox txtLicensePlate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ PARKING/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PARKING
+{
+    public partial class Form1 : Form
+    {
+        // Use a list of dictionaries to store data with Slot, License Plate, and Status
+        List<Dictionary<string, string>> parkingData = new List<Dictionary<string, string>>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Set up DataGridView columns
+            dgvParkingQueue.ColumnCount = 3;
+            dgvParkingQueue.Columns[0].Name = "Slot";
+            dgvParkingQueue.Columns[1].Name = "License Plate";
+            dgvParkingQueue.Columns[2].Name = "Status";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }       
+        private void UpdateDataGridView()
+        {
+            dgvParkingQueue.Rows.Clear();
+            foreach (var data in parkingData)
+            {
+                dgvParkingQueue.Rows.Add(data["Slot"], data["License Plate"], data["Status"]);
+            }
+        }
+
+        private void RenumberSlots()
+        {
+            for (int i = 0; i < parkingData.Count; i++)
+            {
+                parkingData[i]["Slot"] = (i + 1).ToString();
+            }
+        }
+        //Helper function to find the next available slot
+        private int FindNextAvailableSlot()
+        {
+            if (parkingData.Count == 0) return 1;
+            return parkingData.Max(x => int.Parse(x["Slot"])) + 1;
+        }
+
+        private void btnAddVehicle_Click_1(object sender, EventArgs e)
+        {
+            string licensePlate = txtLicensePlate.Text;
+            if (string.IsNullOrEmpty(licensePlate))
+            {
+                MessageBox.Show("Please enter a License Plate.");
+                return;
+            }
+
+            // Find the next available slot
+            int nextSlot = FindNextAvailableSlot();
+
+            // Add data to parkingData
+            parkingData.Add(new Dictionary<string, string>()
+            {
+                { "Slot", nextSlot.ToString() },
+                { "License Plate", licensePlate },
+                { "Status", "Waiting" }
+            });
+
+            UpdateDataGridView();
+            txtLicensePlate.Text = "";
+        }
+
+        private void btnRemoveVehicle_Click(object sender, EventArgs e)
+        {
+            string licensePlateToRemove = txtLicensePlate.Text;
+            if (string.IsNullOrEmpty(licensePlateToRemove))
+            {
+                MessageBox.Show("Please enter the license plate to remove.");
+                return;
+            }
+
+            int indexToRemove = parkingData.FindIndex(x => x["License Plate"] == licensePlateToRemove);
+
+            if (indexToRemove != -1)
+            {
+                parkingData.RemoveAt(indexToRemove);
+                RenumberSlots(); // Call the renumbering function
+                UpdateDataGridView();
+                txtLicensePlate.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("License plate not found in the queue.");
+            }
+        }
+
+        private void btnClearQueue_Click_1(object sender, EventArgs e)
+        {
+            parkingData.Clear();
+            UpdateDataGridView();
+        }
+    }
+}
+
+
+  
+    
